a3d47d5340593fc1f7060f8fe8e8cb75220c68b8
Azazel <40333954+InfernalAzazel@users.noreply.github.com>
2021-09-23 07:13:22 +0800

    更新代码

--- examples/jd.rs
+++ examples/jd.rs
@@ -1,20 +1,9 @@
-use serde_json::Value;
-use std::collections::HashMap;
+use serde_json::json;
 use yetai_rs::jdy;
 
 #[tokio::main]
 async fn main() {
-    // let jd = JDSDK::new(
-    //     "5dde829086f77b0006f3833e".to_string(),
-    //     "60dd8e24224ed700089fbe49".to_string(),
-    //     "Q20Prk3r78ih4w0ZYOr6iEFfj9g6cEk0".to_string(),
-    // );
-    // let arr = jd.get_widgets().await;
-    // if let Some(arr) = arr.as_array() {
-    //     for x in arr {
-    //         println!("测试 {}", x.to_string());
-    //     }
-    // }
+
 
     let cfg = jdy::Config::new(
         "5dde829086f77b0006f3833e".to_string(),
@@ -22,14 +11,21 @@ async fn main() {
         "Q20Prk3r78ih4w0ZYOr6iEFfj9g6cEk0".to_string(),
     );
 
-    let arr = jdy::get_widgets(cfg).await;
+    // let arr = jdy::get_widgets(cfg).await;
+    // if let Some(arr) = arr.as_array() {
+    //     for x in arr {
+    //         println!("测试 {}", x.to_string());
+    //     }
+    // }
+
+    let arr = jdy::get_all_data(cfg, vec![], json!({})).await;
     if let Some(arr) = arr.as_array() {
         for x in arr {
             println!("测试 {}", x.to_string());
         }
     }
 
-    // let arr = jd.get_form_data("", 10, [].to_vec(), Value::from({})).await;
+    // let arr = jdy::get_form_data(&cfg, "", 10, [].to_vec(), json!({})).await;
     // if let Some(arr) = arr.as_array(){
     //     for x in arr {
     //         println!("测试2 {}", x.to_string());
--- src/jdy.rs
+++ src/jdy.rs
@@ -1,7 +1,10 @@
-use crate::request::send_jdy_post;
 use reqwest::header::HeaderMap;
 use serde_json::json;
 use serde_json::Value;
+use std::borrow::Borrow;
+use std::collections::HashMap;
+use std::fmt::format;
+
 
 pub struct Config {
     url_get_widgets: String,
@@ -11,9 +14,6 @@ pub struct Config {
     url_create_data: String,
     url_delete_data: String,
     api_key: String,
-}
-struct Context {
-    form_data: Vec<Value>,
 }
 
 impl Config {
@@ -27,7 +27,7 @@ impl Config {
 
         Config {
             url_get_widgets: set_method("v1".to_string(), "widgets".to_string()),
-            url_get_data: set_method("v1".to_string(), "data".to_string()),
+            url_get_data: set_method("v2".to_string(), "data".to_string()),
             url_retrieve_data: set_method("v1".to_string(), "data_retrieve".to_string()),
             url_update_data: set_method("v1".to_string(), "data_update".to_string()),
             url_create_data: set_method("v1".to_string(), "data_create".to_string()),
@@ -47,6 +47,36 @@ impl Config {
     }
 }
 
+async fn send_jdy_post(
+    url: String,
+    data: Value,
+    headers: HeaderMap,
+) -> Result<HashMap<String, Value>, reqwest::Error> {
+    // 创建client
+    let client = reqwest::Client::new();
+
+    let res = client
+        .post(url)
+        .headers(headers.clone())
+        .json(&data)
+        .send()
+        .await?;
+
+    let status = &res.status();
+
+    let result: HashMap<String, Value> = res.json().await?;
+
+    if status.as_u16() >= 400 {
+        if result["code"] == 8303 && true {
+            Ok((result))
+        } else {
+            println!("err: {:?}", result);
+            panic!()
+        }
+    } else {
+        Ok((result))
+    }
+}
 pub async fn get_widgets(cfg: Config) -> Value {
     send_jdy_post(
         format!("{}", cfg.url_get_widgets),
@@ -59,12 +89,13 @@ pub async fn get_widgets(cfg: Config) -> Value {
 }
 
 pub async fn get_form_data(
-    cfg: Config,
+    cfg: &Config,
     data_id: &str,
     limit: i32,
     fields: Vec<String>,
     filter: Value,
 ) -> Value {
+    println!("请求的 data_id => {:?}",data_id);
     send_jdy_post(
         format!("{}", cfg.url_get_data),
         json!(
@@ -82,24 +113,29 @@ pub async fn get_form_data(
     .unwrap_or_default()
 }
 
-// async fn get_next_page(cfg: Config, data_id: &str, fields: Vec<String>, filter: Value) ->  Value{
-//     let mut form_data: Vec<Value> = Vec::new();
-//     let mx = |data_id| async {
-//         let data = get_form_data(cfg, data_id, 100, fields.clone(), filter.clone()).await;
-//         if let Some(data) = data.as_array(){
-//             for v in data{
-//                 form_data.append( &mut vec![]);
-//                 let new_data_id = data[data.len() - 1]["_id"].clone();
-//                 if let Some(new_data_id) = new_data_id.as_str() {
-//                 }
-//             }
-//         }
-//     };
-//      json!(form_data.clone())
-// }
-// pub async fn get_all_data(cfg: Config, fields: Vec<String>, filter: Value) -> Value {
-//     get_next_page(cfg,&mut vec![], "", fields, filter).await
-// }
+pub async fn get_all_data(cfg: Config, fields: Vec<String>, filter: Value) -> Value {
+    let mut data_list: Vec<Value> = Vec::new();
+    let mut data_id = String::new();
+    let mut  data :Value = Value::default();
+    loop {
+
+        data = get_form_data(&cfg, &data_id, 2, fields.clone(), filter.clone()).await;
+        if let Value::Array(mut data) = data {
+                // 获取最后一条数据的id
+                match data.last().map(|v| v.as_object()) {
+                    None => break,
+                    Some(res) => {
+                        if let Some(res) = res{
+                             data_id = res["_id"].as_str().unwrap().to_string();
+                             println!("{:?}", res["msg"]);
+                        }
+                        data_list.append(&mut data)
+                    }
+                };
+            }
+    }
+    return json!(data_list);
+}
 
 pub async fn retrieve_data(cfg: Config, data_id: &str) -> Value {
     send_jdy_post(
@@ -129,7 +165,7 @@ pub async fn create_data(cfg: Config, data: Value) -> Value {
     .map(|res| json!(res["data"]))
     .unwrap_or_default()
 }
-pub async fn update_data(cfg: Config,  data_id: &str, data: Value) -> Value {
+pub async fn update_data(cfg: Config, data_id: &str, data: Value) -> Value {
     send_jdy_post(
         format!("{}", cfg.url_update_data),
         json!(
