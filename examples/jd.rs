2c744441b0dfccebf595f9a472a9db738fa78f6a
Azazel <40333954+InfernalAzazel@users.noreply.github.com>
2021-10-02 20:23:57 +0800

    更新

--- examples/jd.rs
+++ examples/jd.rs
@@ -1,40 +1,63 @@
 use serde_json::json;
 use yetai_rs::jdy;
+use core::result::Result;
 
 #[tokio::main]
 async fn main() {
-
-
-    let cfg = jdy::Config::new(
+    let form = jdy::JDY::new(
         "5dde829086f77b0006f3833e".to_string(),
         "60dd8e24224ed700089fbe49".to_string(),
         "Q20Prk3r78ih4w0ZYOr6iEFfj9g6cEk0".to_string(),
     );
 
-    // let arr = jdy::get_widgets(cfg).await;
-    // if let Some(arr) = arr.as_array() {
-    //     for x in arr {
-    //         println!("测试 {}", x.to_string());
-    //     }
-    // }
-
-    let arr = jdy::get_all_data(cfg, vec![], json!({})).await;
-    if let Some(arr) = arr.as_array() {
-        for x in arr {
-            println!("测试 {}", x.to_string());
+    match form.get_widgets().await {
+        Ok(v) => {
+            if let Some(v) = v.as_array() {
+                for x in v {
+                    println!("测试 {}", x.to_string());
+                }
+            }
+        }
+        Err(e) => {
+            println!("{}", e)
         }
     }
+    for _ in 0..2 {
+        match form.get_all_data(vec![], json!({})).await {
+            Ok(v) => {
+                if let Some(v) = v.as_array() {
+                    for x in v {
+                        println!("测试 {}", x.to_string());
+                    }
+                }
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    }
+    let res = match form.create_data( json!({
+        "msg":{"value":"99999999999"}
+    })).await {
+         Ok(v) => {
+             v
+         }
+         Err(e) => {
+             println!("{}", e);
 
-    // let arr = jdy::get_form_data(&cfg, "", 10, [].to_vec(), json!({})).await;
-    // if let Some(arr) = arr.as_array(){
-    //     for x in arr {
-    //         println!("测试2 {}", x.to_string());
-    //     }
-    // }
-    // let arr = jd.get_all_data( [].to_vec(), Value::from({})).await;
-    // if let Some(arr) = arr.as_array(){
-    //     for x in arr {
-    //         println!("测试3 {}", x.to_string());
-    //     }
-    // }
+         }
+
+     };
+    println!("{:?}", res)
+
+}
+
+async fn business() {
+    let form = jdy::JDY::new(
+        "5dde829086f77b0006f3833e".to_string(),
+        "60dd8e24224ed700089fbe49".to_string(),
+        "Q20Prk3r78ih4w0ZYOr6iEFfj9g6cEk0".to_string(),
+    );
+
+    res = form.get_widgets().await?
 }
--- src/jdy.rs
+++ src/jdy.rs
@@ -1,12 +1,11 @@
+use async_recursion::async_recursion;
+use core::result::Result;
 use reqwest::header::HeaderMap;
 use serde_json::json;
 use serde_json::Value;
-use std::borrow::Borrow;
 use std::collections::HashMap;
-use std::fmt::format;
 
-
-pub struct Config {
+pub struct JDY {
     url_get_widgets: String,
     url_get_data: String,
     url_retrieve_data: String,
@@ -16,8 +15,9 @@ pub struct Config {
     api_key: String,
 }
 
-impl Config {
-    pub fn new(app_id: String, entry_id: String, api_key: String) -> Config {
+impl JDY {
+
+    pub fn new(app_id: String, entry_id: String, api_key: String) -> JDY {
         let set_method = |v, method| {
             format!(
                 "https://api.jiandaoyun.com/api/{}/app/{}/entry/{}/{}",
@@ -25,7 +25,7 @@ impl Config {
             )
         };
 
-        Config {
+        JDY {
             url_get_widgets: set_method("v1".to_string(), "widgets".to_string()),
             url_get_data: set_method("v2".to_string(), "data".to_string()),
             url_retrieve_data: set_method("v1".to_string(), "data_retrieve".to_string()),
@@ -35,163 +35,174 @@ impl Config {
             api_key: api_key.to_string(),
         }
     }
-    pub fn get_req_header(&self) -> HeaderMap {
-        // 组装header
+
+    #[async_recursion]
+    async fn post(
+        &self,
+        url: String,
+        data: Value,
+    ) -> Result<HashMap<String, Value>, String> {
         let mut headers = HeaderMap::new();
         headers.insert("Content-Type", "application/json".parse().unwrap());
         headers.insert(
             "Authorization",
             format!("Bearer {}", self.api_key).parse().unwrap(),
         );
-        headers
-    }
-}
 
-async fn send_jdy_post(
-    url: String,
-    data: Value,
-    headers: HeaderMap,
-) -> Result<HashMap<String, Value>, reqwest::Error> {
-    // 创建client
-    let client = reqwest::Client::new();
-
-    let res = client
-        .post(url)
-        .headers(headers.clone())
-        .json(&data)
-        .send()
-        .await?;
-
-    let status = &res.status();
-
-    let result: HashMap<String, Value> = res.json().await?;
-
-    if status.as_u16() >= 400 {
-        if result["code"] == 8303 && true {
-            Ok((result))
-        } else {
-            println!("err: {:?}", result);
-            panic!()
-        }
-    } else {
-        Ok((result))
-    }
-}
-pub async fn get_widgets(cfg: Config) -> Value {
-    send_jdy_post(
-        format!("{}", cfg.url_get_widgets),
-        json!({}),
-        cfg.get_req_header(),
-    )
-    .await
-    .map(|res| json!(res["widgets"]))
-    .unwrap_or_default()
-}
-
-pub async fn get_form_data(
-    cfg: &Config,
-    data_id: &str,
-    limit: i32,
-    fields: Vec<String>,
-    filter: Value,
-) -> Value {
-    println!("请求的 data_id => {:?}",data_id);
-    send_jdy_post(
-        format!("{}", cfg.url_get_data),
-        json!(
-            {
-                "data_id": data_id,
-                "limit": limit,
-                "fields": fields,
-                "filter": filter
+        // 创建client
+        let client = reqwest::Client::new();
+        match client
+            .post(url.clone())
+            .headers(headers.clone())
+            .json(&data)
+            .send()
+            .await
+        {
+            Ok(resp) => {
+                let status = resp.status().clone();
+                match resp.json::<HashMap<String, Value>>().await {
+                    Ok(v) => {
+                        if status.as_u16() >= 400 {
+                            if v["code"] == 8303 && true {
+                                self.post(url.clone(), data.clone()).await
+                            } else {
+                                Err(format!("{:?}", v))
+                            }
+                        } else {
+                            Ok(v)
+                        }
+                    }
+                    Err(e) => Err(format!("err: {:?}", e)),
+                }
             }
-        ),
-        cfg.get_req_header(),
-    )
-    .await
-    .map(|res| json!(res["data"]))
-    .unwrap_or_default()
-}
+            Err(e) => Err(format!("err: {:?}", e)),
+        }
+    }
 
-pub async fn get_all_data(cfg: Config, fields: Vec<String>, filter: Value) -> Value {
-    let mut data_list: Vec<Value> = Vec::new();
-    let mut data_id = String::new();
-    let mut  data :Value = Value::default();
-    loop {
+    pub async fn get_widgets(&self) -> Result<Value, String> {
+        match self.post(
+            format!("{}", self.url_get_widgets),
+            json!({}),
+        )
+        .await
+        {
+            Ok(v) => Ok(json!(v["widgets"])),
+            Err(e) => Err(format!("err: {:?}", e)),
+        }
+    }
 
-        data = get_form_data(&cfg, &data_id, 2, fields.clone(), filter.clone()).await;
-        if let Value::Array(mut data) = data {
+    pub async fn get_form_data(
+        &self,
+        data_id: &str,
+        limit: i32,
+        fields: Vec<String>,
+        filter: Value,
+    ) -> Result<Value, String> {
+        match self.post(
+            format!("{}", self.url_get_data),
+            json!(
+                {
+                    "data_id": data_id,
+                    "limit": limit,
+                    "fields": fields,
+                    "filter": filter
+                }
+            ),
+        )
+        .await
+        {
+            Ok(v) => Ok(json!(v["data"])),
+            Err(e) => Err(format!("err: {:?}", e)),
+        }
+    }
+
+    pub async fn get_all_data(&self, fields: Vec<String>, filter: Value) -> Result<Value, String> {
+        let mut data_list: Vec<Value> = Vec::new();
+        let mut data_id = String::new();
+
+        loop {
+            let data = self
+                .get_form_data(&data_id, 100, fields.clone(), filter.clone())
+                .await?;
+            if let Value::Array(mut data) = data {
                 // 获取最后一条数据的id
                 match data.last().map(|v| v.as_object()) {
                     None => break,
                     Some(res) => {
-                        if let Some(res) = res{
-                             data_id = res["_id"].as_str().unwrap().to_string();
-                             println!("{:?}", res["msg"]);
+                        if let Some(res) = res {
+                            data_id = res["_id"].as_str().unwrap().to_string();
+                            //println!("{:?}", res["msg"]);
                         }
                         data_list.append(&mut data)
                     }
                 };
             }
+        }
+        Ok(json!(data_list))
     }
-    return json!(data_list);
-}
 
-pub async fn retrieve_data(cfg: Config, data_id: &str) -> Value {
-    send_jdy_post(
-        format!("{}", cfg.url_retrieve_data),
-        json!(
-            {
-                "data_id": data_id,
-            }
-        ),
-        cfg.get_req_header(),
-    )
-    .await
-    .map(|res| json!(res["data"]))
-    .unwrap_or_default()
-}
-pub async fn create_data(cfg: Config, data: Value) -> Value {
-    send_jdy_post(
-        format!("{}", cfg.url_create_data),
-        json!(
-            {
-                "data": data,
-            }
-        ),
-        cfg.get_req_header(),
-    )
-    .await
-    .map(|res| json!(res["data"]))
-    .unwrap_or_default()
-}
-pub async fn update_data(cfg: Config, data_id: &str, data: Value) -> Value {
-    send_jdy_post(
-        format!("{}", cfg.url_update_data),
-        json!(
-            {
-                "data_id": data_id,
-                "data": data
-            }
-        ),
-        cfg.get_req_header(),
-    )
-    .await
-    .map(|res| json!(res["data"]))
-    .unwrap_or_default()
-}
+    pub async fn retrieve_data(&self, data_id: &str) -> Result<Value, String> {
+        match self.post(
+            format!("{}", self.url_retrieve_data),
+            json!(
+                {
+                    "data_id": data_id,
+                }
+            ),
+        )
+        .await
+        {
+            Ok(v) => Ok(json!(v["data"])),
+            Err(e) => Err(format!("err: {:?}", e)),
+        }
+    }
 
-pub async fn delete_data(cfg: Config, data_id: &str) -> Value {
-    send_jdy_post(
-        format!("{}", cfg.url_delete_data),
-        json!(
-            {
-                "data_id": data_id,
-            }
-        ),
-        cfg.get_req_header(),
-    )
-    .await
-    .map(|res| json!(res["data"]))
-    .unwrap_or_default()
+    pub async fn create_data(&self, data: Value) -> Result<Value, String> {
+        match self.post(
+            format!("{}", self.url_create_data),
+            json!(
+                {
+                    "data": data,
+                }
+            ),
+        )
+        .await
+        {
+            Ok(v) => Ok(json!(v["data"])),
+            Err(e) => Err(format!("err: {:?}", e)),
+        }
+    }
+
+    pub async fn update_data(&self, data_id: &str, data: Value) -> Result<Value, String> {
+        match self.post(
+            format!("{}", self.url_update_data),
+            json!(
+                {
+                    "data_id": data_id,
+                    "data": data
+                }
+            ),
+        )
+        .await
+        {
+            Ok(v) => Ok(json!(v["data"])),
+            Err(e) => Err(format!("err: {:?}", e)),
+        }
+    }
+
+    pub async fn delete_data(&self, data_id: &str) -> Result<Value, String> {
+        match self.post(
+            format!("{}", self.url_delete_data),
+            json!(
+                {
+                    "data_id": data_id,
+                }
+            ),
+        )
+        .await
+        {
+            Ok(v) => Ok(json!(v["data"])),
+            Err(e) => Err(format!("err: {:?}", e)),
+        }
+    }
 }
