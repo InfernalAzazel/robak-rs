ae1d8f940bcf9452c027fb7431f890c7c5e607a7
kylin <40333954+InfernalAzazel@users.noreply.github.com>
2021-11-04 15:08:52 +0800

    Update and rename jd.rs to main.rs

--- examples/main.rs
+++ examples/main.rs
@@ -4,10 +4,11 @@ use core::result::Result;
 
 #[tokio::main]
 async fn main() {
+    // 输入自己的key
     let form = jdy::JDY::new(
-        "5dde829086f77b0006f3833e".to_string(),
-        "60dd8e24224ed700089fbe49".to_string(),
-        "Q20Prk3r78ih4w0ZYOr6iEFfj9g6cEk0".to_string(),
+        "".to_string(),
+        "".to_string(),
+        "".to_string(),
     );
 
     match form.get_widgets().await {
